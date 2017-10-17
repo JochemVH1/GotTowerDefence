b9df50d4532a0290eacfd9cfe1711b1235e312b5
JochemVH1 <jochem.vanhespen.w1504@student.hogent.be>
2017-10-17 16:48:22 +0300

    added basic movement in level1

--- assets/Script/Level1.js
+++ assets/Script/Level1.js
@@ -21,10 +21,18 @@ cc.Class({
     // use this for initialization
     onLoad: function () {
 		var node = new cc.Node('Sprite');
+		node.setAnchorPoint(0,0);
+		node.setPosition(-500,230);
 		var sp = node.addComponent(cc.Sprite);
-
 		sp.spriteFrame = this.sprite;
 		node.parent = this.node;
+		var move_moveBY_1 = cc.MoveBy.create(3,cc.p(375,0));
+		var move_moveBY_2 = cc.MoveBy.create(3,cc.p(0,-440));
+		var move_moveBY_3 = cc.MoveBy.create(3,cc.p(470,0));
+		var move_moveBY_4 = cc.MoveBy.create(3,cc.p(0,255));
+		var move_moveBY_5 = cc.MoveBy.create(2,cc.p(200,0));
+		var path = cc.sequence(move_moveBY_1,move_moveBY_2,move_moveBY_3,move_moveBY_4,move_moveBY_5);
+		node.runAction(path);
 
     },
 
